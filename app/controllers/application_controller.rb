d3b07b77c320ed53302417200478e1cc36076702
erikzaksf <erik.zak.sf@gmail.com>
2017-12-20 09:53:19 -0800

    add IP address blocking, list IP's in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :is_admin?
+
+  before_action :block_ip_addresses
+
+  protected
+
+  def block_ip_addresses
+    head :unauthorized if current_ip_address == "0.0.0.0"
+  end
+
+  def current_ip_address
+    request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
+  end
 end
