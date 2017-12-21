1b52a5b69cb5b0dfc9fb2687d5d0e193252fbcd0
erikzaksf <erik.zak.sf@gmail.com>
2017-12-21 12:53:06 -0800

    add order to index page, now shows 10 most recent

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
+  scope :ten_most_recent, -> { order(updated_at: :desc).limit(10)}
+
+  # scope :desc, -> {order (updated_at: :desc)}
+
   mount_uploader :image, ImageUploader
 
   validates :title, :body, :user_id, presence: true
